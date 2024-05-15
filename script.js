65536ea5a1d0d447ceb2d7294507a4330a04bd13
usuanum70 <166126468+usuanum70@users.noreply.github.com>
2024-05-15 11:13:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function jeans(){
+    var expandir = document.getElementById("jeans");
+    if (expandir.style.display === "none") {
+        expandir.style.display = "block";
+    }else {
+        expandir.style.display = "none";
+    }
+}
+function abrasidebar() {
+    var abra = document.getElementById("sidebar");
+    if (abra.style.display === "block") {
+        abra.style.display = "none";
+    }else {
+        abra.style.display = "block";
+    }
+}
+function overlay() {
+    var abra = document.getElementById("overlay");
+    if (abra.style.display === "block") {
+        abra.style.display = "none";
+    }else {
+        abra.style.display = "block";
+    }
+}
